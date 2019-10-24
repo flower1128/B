ccaae0ec9db29986f1d25107ffea0068162d39d3
flower <flower@163.com>
2019-10-24 16:12:44 +0800

    创建了HelloGit

--- HelloGit/src/com/turing/c.java
+++ HelloGit/src/com/turing/c.java
@@ -2,7 +2,7 @@ package com.turing;
 
 public class c {
     public static void main(String[] args) {
-        System.out.print("cccc ");
+        System.out.print("ccccc");
         System.out.print("aaaaaac");
     }
 }
