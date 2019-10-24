3b2b11e3cc8efc0a91c8b42255eeb60ccaca4437
flower <flower@163.com>
2019-10-24 16:16:39 +0800

    创建了HelloGit

--- HelloGit/src/com/turing/Main.java
+++ HelloGit/src/com/turing/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.print("AAAAAAAAAAAAvv2");
-        System.out.print("AAAAAAAAAAAAvv3aaa");
     }
 }
